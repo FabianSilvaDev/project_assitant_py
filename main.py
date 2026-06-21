ceaed5d77b05bdbd4ddaabd70c6b3370429fced5
FabianSilvaDev <produccionesbog@gmail.com>
2026-06-21 11:13:56 -0500

    init with memory central for assitant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import subprocess
 
+print("preparando el asistente...")
+memory = []
+
 while True:
 
     message = input("Enter your message: ")
@@ -11,7 +14,35 @@ while True:
 
     #strip para eliminar espacios en blanco al inicio y al final
     response = traduction_assitant.strip()
-    print(response)
 
+    #__________________________________________________________________________________________________________
+
+
+    #guardamos el mensaje y la respueta para inicializar el contexto de la conversación, y así el asistente pueda recordar lo que se ha dicho anteriormente.
+    memory.append((message, response))
+
+    #si la memoria supera los 10 mensajes, eliminamos el más antiguo para mantener un contexto relevante y evitar que la memoria se vuelva demasiado grande.
+    if len(memory) > 10:
+        memory.pop(10)
+
+    #__________________________________________________________________________________________________________
+
+    funtions = {
+        'show_memory': lambda: print(memory)
+    }
+
+    if message == 'muestrame la memoria':
+        funtions['show_memory']()
+    else:
+         # imprimimos respuesta
+        print(response)
+
+   
+
+    #ejecutamos un break para salir del bucle si el usuario escribe 'adios'
     if message == 'adios':
-        break
+         break
+
+
+    
+
